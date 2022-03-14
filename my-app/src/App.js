5faee438d048c78b5d5f4a4017cd6bf426fc73b3
Vikram <vikramamin22@gmail.com>
2022-03-14 21:48:48 +0530

    Vegitable part is Done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Wishlist from './Components/Wishlist';
+import Vegetables from './Components/Vegetables';
 
 function App() {
   return (
    <>
    <div className="container">
     <Wishlist/>
+    <hr />
+    <Vegetables/>
     </div>
 
     </>
---
+++ my-app/src/Components/Vegetables.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+function Vegetables() {
+
+    let vegitablesList = [
+        {
+        id :0,
+        name : "tomatoes",
+        quantity : 10,
+     },
+     {
+        id :1,
+        name : "potatoes",
+        quantity : 8,
+     },
+     {
+        id :2,
+        name : "carrots",
+        quantity : 5,
+     },
+     {
+        id :3,
+     name : "onions",
+     quantity : 7,
+  }
+ ]
+
+ const [items,setItems] = React.useState(vegitablesList);
+
+
+ function increment(id){
+    // console.log(id)
+  let newItems =[...items];
+  newItems[id] = {...items[id], quantity:items[id].quantity+1};
+  setItems(newItems)
+}
+
+function decrement(id){
+  //  console.log(id)
+ let newItems =[...items];
+ newItems[id] = {...items[id],quantity:items[id].quantity-1
+ };
+
+ setItems(newItems)
+}
+
+
+  return (
+      <>
+    <div>Vegetables</div>
+    {
+       items.map(({name,quantity,id}) => 
+       <div> 
+       <div >{`${name} - ${quantity}`}</div>
+       <button counts={quantity} onClick={ () => increment(id)} >+</button>
+       <button counts={quantity} onClick={ () => decrement(id)} >-</button>
+     
+       </div>
+
+      
+       
+       )
+       
+    }
+    </>
+  )
+}
+
+export default Vegetables
